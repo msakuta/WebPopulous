34587c5979db72734e64efa65a6ef7a6e27b3358
msakuta <masahiro.sakuta@gmail.com>
2014-07-18 02:06:40 +0900

    Initial commit

---
+++ PopGame.js
@@ -0,0 +1,147 @@
+
+function PopGame(xs,ys){
+	this.xs = xs;
+	this.ys = ys;
+	this.rng = new Xor128();
+
+	this.cells = [];
+
+	this.workingPower = 100;
+	this.cash = 100;
+	this.weather = 0;
+	this.time = 0;
+	this.frameCount = 0;
+	this.autosave_frame = 0;
+}
+
+PopGame.Cell = function(height){
+	this.height = height;
+}
+
+
+
+PopGame.prototype.init = function(){
+//	if(typeof(Storage) !== "undefined"){
+//		this.deserialize(localStorage.getItem("PopGameSave"));
+//	}
+//	else
+	{
+		for(var x = 0; x < this.xs; x++){
+			var row = [];
+			for(var y = 0; y < this.ys; y++){
+				var vx = x - 5;
+				var vy = y - 5;
+				var height = vx * vx + vy * vy < 3 * 3 ? 2 : vx * vx + vy * vy < 5 * 5 ? 1 : 0;
+				var cell = new PopGame.Cell(height);
+
+				this.onUpdateCell(cell,x,y);
+
+				row.push(cell);
+			}
+			this.cells.push(row);
+		}
+	}
+}
+
+PopGame.prototype.cellAt = function(x,y){
+	if(x < 0 || this.cells.length <= x || y < 0 || this.cells[x].length <= y)
+		return new PopGame.Cell(0);
+	return this.cells[x][y];
+}
+
+PopGame.prototype.isFlat = function(x,y){
+	var c = this.cellAt(x, y);
+	var cl = this.cellAt(x-1, y);
+//	var cr = this.cellAt(x+1, y);
+	var cu = this.cellAt(x, y-1);
+//	var cd = this.cellAt(x, y+1);
+	return cl && cl.height === c.height /*&& cr && cr.height === c.height*/
+		&& cu && cu.height === c.height /*&& cd && cd.height === c.height*/;
+}
+
+PopGame.prototype.slopeID = function(x,y){
+	if(x < 0 || this.cells.length <= x || y < 0 || this.cells[x].length <= y){
+//		console.log("ob: %d, %d [%d, %d]\n", x, y, this.cells.length-1, this.cells[0].length-1);
+		return 0;
+	}
+	var maxh = 0;
+	var minh = 127;
+	for(var xx = x; xx <= x+1; xx++) for(var yy = y; yy <= y+1; yy++){
+		var h = this.cellAt(xx, yy).height;
+		if(maxh < h)
+			maxh = h;
+		if(h < minh)
+			minh = h;
+	}
+	if(minh + 1 < maxh)
+		return 0xf;
+	var ret = (this.cellAt(x, y).height - minh ? 1 : 0)
+		| (this.cellAt(x+1, y).height - minh ? 2 : 0)
+		| (this.cellAt(x, y+1).height - minh ? 4 : 0)
+		| (this.cellAt(x+1, y+1).height - minh ? 8 : 0);
+//	console.log("min = %d, max = %d, %d\n", minh, maxh, ret);
+	return ret;
+}
+
+
+PopGame.prototype.onUpdateCell = function(cell,x,y){}
+
+PopGame.prototype.update = function(deltaTime){
+	var frameTime = 100; // Frame time interval in milliseconds
+	this.time += deltaTime;
+
+	// Repeat the frame procedure in constant interval.
+	while(frameTime < this.time){
+
+		this.updateInternal();
+
+		this.time -= frameTime;
+	}
+}
+
+/// Simple random number generator.
+function RandomSequence(seed){
+	this.z = (seed & 0xffffffff) + 0x7fffffff;
+	this.w = (((this.z ^ 123459876) * 123459871) & 0xffffffff) + 0x7fffffff;
+}
+
+RandomSequence.prototype.nexti = function(){
+	return ((((this.z=36969*(this.z&65535)+(this.z>>16))<<16)+(this.w=18000*(this.w&65535)+(this.w>>16))) & 0xffffffff) + 0x7fffffff;
+}
+
+RandomSequence.prototype.next = function(){
+	return this.nexti() / 0xffffffff;
+}
+
+/// Noise with a low frequency which is realized by interpolating polygon chart.
+function smoothNoise(i){
+	var seed = 123;
+	var period = 600; // one minute
+	var sum = 0.;
+	for(var j = 0; j <= 1; j++){
+		var rng = new RandomSequence(Math.floor(i / period) + j);
+		var value = rng.next(rng);
+		// Uniformly distributed random variable is squared to make rainy weather have lower probability.
+		sum += value * value * (j ? i % period : period - i % period) / period;
+	}
+	return sum;
+}
+
+PopGame.prototype.updateInternal = function(){
+	for(var x = 0; x < this.cells.length; x++){
+		for(var y = 0; y < this.cells[x].length; y++){
+			var cell = this.cells[x][y];
+			
+			if(x % 2 == 0 && y % 2 == 0 && 10 < x){
+				cell.height = this.rng.nexti() % 2;
+				this.cellAt(x-1, y).height = cell.height;
+				this.cellAt(x-1, y-1).height = cell.height;
+				this.cellAt(x, y-1).height = cell.height;
+			}
+
+			game.onUpdateCell(cell,x,y);
+		}
+	}
+
+}
+
---
+++ populous.js
@@ -0,0 +1,89 @@
+
+var width;
+var height;
+var stage;
+var game;
+
+window.onload = function(){
+	var canvas = document.getElementById("stage");
+	width = parseInt(canvas.style.width);
+	height = parseInt(canvas.style.height);
+
+	stage = new createjs.Stage("stage");
+
+	init();
+}
+
+var groundBaseTexture;
+
+function init(){
+	game = new PopGame(width / 32, height / 32);
+
+	groundBaseTexture = new createjs.SpriteSheet({
+		images: ["assets/grass.png"],
+		frames: {width: 32, height: 32},
+	});
+
+	var groundTexture = new createjs.Sprite(groundBaseTexture, 0);
+
+	var ground = new createjs.Container();
+	stage.addChild(ground);
+
+	game.init();
+
+	game.onUpdateCell = function(cell,x,y){
+		if(cell.graphics == undefined){
+			cell.graphics = new createjs.Container();
+
+			ground.addChild(cell.graphics);
+		}
+		if(cell.gs == undefined){
+			var groundSprite = groundTexture.clone();
+			cell.graphics.addChild(groundSprite);
+			cell.gs = groundSprite;
+		}
+		var sid = this.slopeID(x, y);
+		cell.gs.gotoAndStop(/*this.isFlat(x, y) ? 0 : */sid);
+		cell.gs.x = width / 2 - 8 + x * 16 - y * 16;
+		cell.gs.y = x * 8 + y * 8 - cell.height * 8 - (sid & 1 ? 8 : 16);
+	}
+
+//	var cursorSprite = new createjs.Bitmap("assets/grass.png");
+//	stage.addChild(cursorSprite);
+
+	var overlay = new createjs.Container();
+
+//	stage.addChild(overlay);
+	requestAnimationFrame(animate);
+
+	// Variable to remember the last time of animation frame.
+	var lastTime = null;
+
+//	createjs.Ticker.addEventListener("tick", animate);
+	function animate(timestamp) {
+		// Calculate the delta-time of this frame for game update process.
+		if(lastTime === null)
+			lastTime = timestamp;
+		var deltaTime = timestamp - lastTime;
+		lastTime = timestamp;
+
+		game.update(deltaTime);
+
+		stage.update();
+
+		requestAnimationFrame(animate);
+	}
+}
+
+function reset(){
+	if(confirm("Are you sure to reset progress?")){
+		localStorage.removeItem("PopGameSave");
+
+		// We want to use clearAll(), but PIXI.Stage does not officially support it.
+		// At least we want removeChildAt() or something...
+		while(stage.children.length != 0)
+			stage.removeChild(stage.getChildAt(0));
+
+		init();
+	}
+}
